5e99a007d936bf0ffd22ac6357e27dd3fc03d7ad
younes <ybelarbi.dev@gmail.com>
2020-12-18 11:03:23 +0100

    implement--> SignIn component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Form, Input, Button, Grid, Segment } from 'semantic-ui-react';
 
-function SignIn() {
+
+class  SignIn extends Component {
+  state = {
+    email:'',
+    password:''
+  }
+
+  handleChange = (e) => {
+    this.setState ({
+      [e.target.id] : e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {    
     return (
-        <div>
-            <h2>signIn</h2>
-        </div>
-    )
+      <Grid container  centered>
+        <Grid.Column >
+          <Segment>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field
+                id='email'
+                control={Input}
+                label='Email'
+                placeholder='joe@schmoe.com'
+                type="email"
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                id='password'
+                control={Input}
+                label='Password'
+                placeholder='password'
+                type= "password"
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                id='form-button-control-public'
+                control={Button}
+                content='Confirm'
+                label='Label with htmlFor'
+              />
+            </Form>
+          </Segment>
+        </Grid.Column>  
+      </Grid>
+    );
+  }
 }
 
-export default SignIn;
+export default SignIn; 
