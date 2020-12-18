1541ce4384c04dd7d19e8d7a78eba267ee85ca4d
younes <ybelarbi.dev@gmail.com>
2020-12-18 09:08:06 +0100

    routing--> done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import {  BrowserRouter, Route, Switch} from "react-router-dom";
 
+
+import Navbar from '../navigation/navbar';
+import SignIn from '../auth/SignIn';
+import SignUp from '../auth/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Projects Manager</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Projects Manager</h1>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/signin" component={SignIn}/>
+          <Route exact path="/signup" component={SignUp}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
