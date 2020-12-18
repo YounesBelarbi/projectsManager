fe359d72291a7daac6fc7b7772046a6cebd57bbe
younes <ybelarbi.dev@gmail.com>
2020-12-18 11:03:43 +0100

    implement--> SignUp component

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Form, Input, Button, Grid, Segment } from 'semantic-ui-react';
 
-function SignUp() {
+
+class  SignUp extends Component {
+  state = {
+    email:'',
+    password:'',
+    username:''
+  }
+
+  handleChange = (e) => {
+    this.setState ({
+      [e.target.id] : e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {    
     return (
-        <div>
-            <h2>signUp</h2>
-        </div>
-    )
+      <Grid container  centered>
+        <Grid.Column >
+          <Segment>  
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field
+                id='email'
+                control={Input}
+                label='Email'
+                placeholder='joe@schmoe.com'
+                type="email"
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                id='password'
+                control={Input}
+                label='Password'
+                placeholder='password'
+                type= "password"
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                id='username'
+                control={Input}
+                label='username'
+                placeholder='username'
+                type= "text"
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                id='form-button-control-public'
+                control={Button}
+                content='Confirm'
+                label='Label with htmlFor'
+              />
+            </Form>
+          </Segment>
+        </Grid.Column>  
+      </Grid>
+    );
+  }
 }
-
-export default SignUp;
+  
+export default SignUp; 
