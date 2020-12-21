ad14394e06ef833f6ea30878f3d54ffd910471b0
younes <ybelarbi.dev@gmail.com>
2020-12-21 21:37:45 +0100

    implement--> handle signIn action in authReducer

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    authError: null
+};
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOGIN_ERROR': {
+            console.log('login error ')
+            return {
+                ...state,
+                authError: 'login failed'
+            }
+        }
+        case 'LOGIN_SUCCESS': {
+            console.log('login success ')
+            return {
+                ...state,
+                authError: null
+            }
+        }    
+        default: {
+            return state
+        }            
+    }    
+}
+
+export default authReducer;
