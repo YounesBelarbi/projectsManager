53d31a3446fbbe8073ec25ff76df39c23ada53bd
younes <ybelarbi.dev@gmail.com>
2020-12-21 21:29:36 +0100

    setting up--> firebase configuratoin and instance

--- src/index.js
+++ src/index.js
@@ -2,15 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import 'firebase/database';
+
 
 import './styles/style.css';
 import App from './components/App';
 import appStore from './store'
-import reportWebVitals from './reportWebVitals';
+
+
+// react-redux-firebase 
+const fbConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+}
+
+// Initialize firebase instance
+firebase.initializeApp(fbConfig)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: appStore.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
 
 ReactDOM.render(
   <Provider store={appStore}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
 );
