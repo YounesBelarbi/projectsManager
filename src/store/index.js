0528fb731969038c4d48a3428319b30c9dd21e17
younes <ybelarbi.dev@gmail.com>
2020-12-21 13:43:59 +0100

    setting up--> thunk middleware and getFirebase, getFirestore in create Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { getFirestore } from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
 
 
-import rootReducer from './rootReducer';
+import rootReducer from './reducers/rootReducer';
 
 
-const appStore = createStore(rootReducer);
-
+const appStore = createStore(
+    rootReducer,
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+    );
 
 export default appStore;
