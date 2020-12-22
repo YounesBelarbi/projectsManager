4c356bba9e505e3fe233a9c181ffd8feb69f5c54
younes <ybelarbi.dev@gmail.com>
2020-12-22 20:41:50 +0100

    fix--> unnecessary import

--- src/components/navigation/signedInLinks.js
+++ src/components/navigation/signedInLinks.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
-
 
 import { signOutAction } from '../../store/actions/authActions';
 
