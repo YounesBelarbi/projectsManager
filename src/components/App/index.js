481eb0acb47137eb6a47686f43ea1c85308564ef
younes <ybelarbi.dev@gmail.com>
2020-12-22 01:59:31 +0100

    implement--> implementation of the display waiting for user authentication

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {  BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
 
 
 import Navbar from '../navigation/navbar';
 import SignIn from '../auth/SignIn';
 import SignUp from '../auth/SignUp';
 
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div></div>;
+  return children
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <h1>Projects Manager</h1>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/signin" component={SignIn}/>
-          <Route exact path="/signup" component={SignUp}/>
-        </Switch>
-      </div>
+      <AuthIsLoaded>            
+        <div className="App">
+          <h1>Projects Manager</h1>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/signin" component={SignIn}/>
+            <Route exact path="/signup" component={SignUp}/>
+          </Switch>
+        </div>
+      </AuthIsLoaded>
     </BrowserRouter>
   );
 }
