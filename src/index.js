66bca9aa55dcb3d6d428f10afd2c2c4a0667a223
younes <ybelarbi.dev@gmail.com>
2020-12-24 20:16:31 +0100

    setting up--> firestore access

--- src/index.js
+++ src/index.js
@@ -7,37 +7,29 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import 'firebase/database';
+import 'firebase/firestore'
 
 
 import './styles/style.css';
 import App from './components/App';
 import appStore from './store'
-
+import {fbConfig} from './components/config/firebaseConfig'
 
 // react-redux-firebase 
-const fbConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
-
 const rrfConfig = {
-  userProfile: 'users'
-  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
   // enableClaims: true // Get custom claims along with the profile
 }
 
 // Initialize firebase instance
 firebase.initializeApp(fbConfig)
+firebase.firestore();
 
 const rrfProps = {
   firebase,
   config: rrfConfig,
-  dispatch: appStore.dispatch
+  dispatch: appStore.dispatch,
   // createFirestoreInstance // <- needed if using firestore
 }
 
