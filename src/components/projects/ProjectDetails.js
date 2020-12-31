aca5486b2bcc6586a117ef57530e0f2c27be9ef4
younes <ybelarbi.dev@gmail.com>
2020-12-31 21:01:47 +0100

    implement--> implementation of ProjectDetails component in class + delete method

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 import { AllowUserIfAuthenticated } from '../auth/authenticationChecker';
+import { deleteProjectAction } from '../../store/actions/projectsActions';
 
 
-const ProjectDetails = ({ project, projectId }) => {
-    if (project) {
-        return (
-            <div>
-                <h3>project detail</h3>
-                <p>{project.content}</p>
-                <Link   
-                to={{
-                    pathname: '/edit/' + projectId,
-                    state: { project: project }
-                }}>
-                    modifier le projet
-                </Link>
-            </div>
-        )
-    } else {
-        return <Redirect to='/dashboard' /> 
+class ProjectDetails extends Component {
+
+    handleDelete = (e) => {
+        this.props.deleteProjectAction( this.props.projectId);
+        this.props.history.push('/dashboard');
+    }
+
+    render() { 
+        if (this.props.project) {
+            return (
+                <div>
+                    <h3>project detail</h3>
+                    <p>{this.props.project.content}</p>
+                    <Link 
+                        to={{
+                            pathname: '/edit/' + this.props.projectId,
+                            state: { project: this.props.project }
+                        }}>
+                        modifier le projet
+                    </Link>
+                    <div>
+                        <button onClick={this.handleDelete}>supprimer le projet </button>
+                    </div>
+                </div>
+            )
+        } else {
+            return <Redirect to='/dashboard' /> 
+        }
     }
 }
 
@@ -38,5 +50,11 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default  AllowUserIfAuthenticated(connect( mapStateToProps, null)(ProjectDetails));
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteProjectAction: (projectId) => dispatch(deleteProjectAction(projectId))
+    }
+}
+
+export default  AllowUserIfAuthenticated(connect( mapStateToProps, mapDispatchToProps)(ProjectDetails));
 
