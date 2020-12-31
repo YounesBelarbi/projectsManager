37a38ba0806dd7826d5dd8f155fda12289c8f168
younes <ybelarbi.dev@gmail.com>
2020-12-31 20:59:13 +0100

    implement--> handle delete action in projectsReducer

--- src/store/reducers/projectsReducer.js
+++ src/store/reducers/projectsReducer.js
@@ -10,10 +10,18 @@ const projectsReducer = (state = initialState, action) => {
             console.log('create project error')
         }     
         case 'UPDATE_PROJECT_SUCCESS': {
+            console.log('update project success')
             return state
         }
         case 'UPDATE_PROJECT_ERROR': {
             console.log('update project error', action.error)
+        }
+        case 'DELETE_PROJECT_SUCCESS': {
+            console.log('delete project success')
+            return state
+        }
+        case 'DELETE_PROJECT_ERROR': {
+            console.log('delete project error', action.error)
         }
         default:
             return state;   
