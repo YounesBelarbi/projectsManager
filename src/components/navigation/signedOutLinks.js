9adf847b6a5cc28ad76a0ca0fb4ead5d066b4862
younes <ybelarbi.dev@gmail.com>
2021-01-03 19:08:06 +0100

    implement--> link and button for all users

--- src/components/navigation/signedOutLinks.js
+++ src/components/navigation/signedOutLinks.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Button, Container, Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 
 const SignedOutLinks = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/signIn">se connecter</Link>
-                </li>
-                <li>
-                    <Link to="/signUp">s'inscrire</Link>
-                </li>
-            </ul>                
-        </div>
+        <Container>
+            <Menu.Item
+                as={NavLink} to="/home"
+                name='Accueil'
+            />        
+            <Menu.Item position='right'>
+                <Button 
+                    as={NavLink} to="/signIn"  
+                    color='blue'
+                    name='signIn'  
+                >
+                    se connecter
+                </Button>
+                <Button 
+                    style={{ marginLeft: '0.5em' }}
+                    as={NavLink} to="/signUp"  
+                    color='blue'
+                    name='signUp' 
+                >
+                    s'inscrire
+                </Button>
+            </Menu.Item>
+      </Container>
     )
 }
 
